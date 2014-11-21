f278c7d7a30c5369ba0acff7ba4156d552349129
arturohv <arturohv@gmail.com>
2014-11-21 13:44:27 -0600

    Script database listo, 24 tablas resultado final

--- Database/gestion.sql
+++ Database/gestion.sql
@@ -1,7 +1,5 @@
-﻿create database gestionpro
-
-create table proyectos(
-	proyectoid serial not null primary key,
+﻿create table proyectos(
+	id serial not null primary key,
 	nombre text not null,
 	empresa text not null,
 	fecha date not null,
@@ -15,75 +13,86 @@ create table proyectos(
 	presupuesto_inicial text not null,
 	requisito_aprobacion text not null
 );
+/*Check FK*/
 
 create table patrocinadores(
-	patrocinadorid serial not null primary key,
+	id serial not null primary key,
 	nombre varchar(60) not null,
 	cargoid int not null,
 	departamentoid int not null,
 	rama_ejecutivaid int not null
 );
+/*Check FK*/
 
 create table patrocinadores_proyectos(
 	id serial not null primary key,
 	proyectoid int not null,
-	patrocidadorid int not null,
+	patrocinadorid int not null,
+	unique(proyectoid,patrocinadorid)
 );
+/*Check FK*/
 
 create table actividades(
-	actividadid serial not null primary key,
+	id serial not null primary key,
 	proyectoid int not null,
 	descripcion text not null,
 	fecha_inicio date not null,
 	fecha_fin date not null	
 );
+/*Check FK*/
 
 create table interesados(
-	interesadoid serial not null primary key,
+	id serial not null primary key,
 	nombre varchar(60) not null,
 	cargoid int not null,
 	departamentoid int not null,
 	rama_ejecutivaid int not null
 );
+/*Check FK*/
 
 create table interesados_proyectos(
 	id serial not null primary key,
 	interesadoid int not null,
-	proyectoid int not null
-); 
+	proyectoid int not null,
+	unique(interesadoid,proyectoid)
+);
+/*Check FK*/
 
 create table departamentos(
-	departamentoid serial not null primary key,
+	id serial not null primary key,
 	nombre varchar(60) not null,
 	descripcion text
 ); 
 
 create table cargos(
-	cargoid serial not null primary key,
+	id serial not null primary key,
 	nombre varchar(60) not null,
 	descripcion text
 );
 
 create table gerentes(
-	gerenteid serial not null primary key,
+	id serial not null primary key,
 	nombre varchar(60) not null,
 	cargoid int not null,
 	departamentoid int not null,
 	rama_ejecutivaid int not null
 );
 
-create table gerentes_proyectos(
+create table gerente_proyecto(
 	id serial not null primary key,
 	gerenteid int not null,
-	proyectoid int not null
+	proyectoid int not null,
+	UNIQUE (gerenteid,proyectoid)
 );
 
-create table gerentes_nivel_autoridad(
+/*Check FK*/
+
+create table gerentes_nivel_autoridad_proyecto(
 	id serial not null primary key,
-	proyectoid int not null,
-	gerenteid int not null,
+	proyectoid int not null,	
 	area_autoridad_id int
 );
+/*Check FK*/
 
 create table areas_autoridad(
 	id serial not null primary key,
@@ -95,35 +104,148 @@ create table areas_autoridad(
 ); 
 
 create table clientes(
-	clienteid serial not null primary key,
+	id serial not null primary key,
 	nombre varchar(60) not null,
-	email nombre varchar(60) not null,
+	email varchar(60) not null,
 	telefono_resi varchar(30) not null,
 	telefono_movil varchar(30) not null,
 	direccion_fisica text
 );
 
-create table clientes_proyectos(
-	id serial not null primary key,
-	clienteid int not null,
-	proyectoid int not null
-);
 
 create table premisas_restricciones(
-	premisaid serial not null primary key,
+	id serial not null primary key,
 	proyectoid int not null,
 	descripcion text
 );
+/*Check FK*/
 
 create table riesgos_iniciales(
-	riesgoid serial not null primary key,
+	id serial not null primary key,
 	proyectoid int not null,
 	descripcion text,
 	fecha_tope date
 );
+/*Check FK*/
+
+create table empleados(
+	id serial not null primary key,
+	nombre varchar(60) not null,
+	cargoid int not null,
+	departamentoid int not null,
+	rama_ejecutivaid int not null
+);
+/*Check FK*/
 
 
-alter table reviews
-  add constraint fk_reviews_products
-  foreign key (productid)
-  references products (productid);
+create table empleados_proyectos(
+	id serial not null primary key,
+	empleadoid int not null,
+	proyectoid int not null,
+	unique(empleadoid,proyectoid)
+);
+/*Check FK*/
+
+
+create table recursos(
+	id serial not null primary key,
+	proyectoid int not null,
+	descripcion varchar(60) not null,
+	cantidad int not null,
+	medidaid int not null	
+);
+/*Check FK*/
+
+
+create table unidad_medidas(
+	id serial not null primary key,
+	simbolo varchar(60) not null,
+	nombre int not null
+);
+
+create table alcances(
+	id serial not null primary key,
+	actividadid int not null,
+	descripcion varchar(60) not null,
+	califid int	not null
+);
+/*Check FK*/
+
+
+create table calificaciones(
+	id serial not null primary key,	
+	descripcion varchar(30) not null
+);
+
+create table costos(
+	id serial not null primary key,
+	alcanceid int not null,
+	descripcion varchar(60) not null,
+	monto money	not null
+);
+/*Check FK*/
+
+create table adquisiciones(
+	id serial not null primary key,
+	alcanceid int not null,
+	descripcion varchar(60) not null,
+	monto money	not null
+);
+/*Check FK*/
+
+create table ramas_ejecutivas(
+	id serial not null primary key,	
+	descripcion varchar(60) not null
+);
+
+
+alter table proyectos add constraint fk_proyectos_clientes foreign key (clienteid) references clientes (id);
+alter table proyectos add constraint fk_proyectos_patrocinadores foreign key (patrocinadorid) references patrocinadores (id);
+alter table proyectos add constraint fk_proyectos_gerentes foreign key (gerenteid) references gerentes (id);
+
+/*1*/alter table patrocinadores add constraint fk_patrocinadores_cargos foreign key (cargoid) references cargos (id);
+alter table patrocinadores add constraint fk_patrocinadores_departamentos foreign key (departamentoid) references departamentos (id);
+alter table patrocinadores add constraint fk_patrocinadores_ramas_ejecutivas foreign key (rama_ejecutivaid) references ramas_ejecutivas (id);
+
+alter table patrocinadores_proyectos add constraint fk_patrocinadores_proyectos_proyectos foreign key (proyectoid) references proyectos (id);
+alter table patrocinadores_proyectos add constraint fk_patrocinadores_proyectos_patrocinadores foreign key (patrocinadorid) references patrocinadores (id);
+
+alter table actividades add constraint fk_actividades_proyectos foreign key (proyectoid) references proyectos (id);
+
+/*2*/alter table interesados add constraint fk_interesados_cargos foreign key (cargoid) references cargos (id);
+alter table interesados add constraint fk_interesados_departamentos foreign key (departamentoid) references departamentos (id);
+alter table interesados add constraint fk_interesados_ramas_ejecutivas foreign key (rama_ejecutivaid) references ramas_ejecutivas (id);
+
+alter table interesados_proyectos add constraint fk_interesados_proyectos_interesados foreign key (interesadoid) references interesados (id); 
+alter table interesados_proyectos add constraint fk_interesados_proyectos_proyectos foreign key (proyectoid) references proyectos (id);
+
+alter table gerentes add constraint fk_gerentes_cargos foreign key (cargoid) references cargos (id);
+alter table gerentes add constraint fk_gerentes_departamentos foreign key (departamentoid) references departamentos (id);
+alter table gerentes add constraint fk_gerentes_ramas_ejecutivas foreign key (rama_ejecutivaid) references ramas_ejecutivas (id);
+
+alter table gerentes_nivel_autoridad_proyecto add constraint fk_gerentes_nivel_autoridad_proyectos foreign key (proyectoid) references proyectos (id);
+alter table gerentes_nivel_autoridad_proyecto add constraint fk_gerentes_nivel_autoridad_areas_autoridad foreign key (area_autoridad_id) references areas_autoridad (id);
+
+alter table premisas_restricciones add constraint fk_premisas_restricciones_proyectos foreign key (proyectoid) references proyectos (id);
+
+alter table riesgos_iniciales add constraint fk_riesgos_iniciales_proyectos foreign key (proyectoid) references proyectos (id);
+
+alter table empleados add constraint fk_empleados_cargos foreign key (cargoid) references cargos (id);
+alter table empleados add constraint fk_empleados_departamentos foreign key (departamentoid) references departamentos (id);
+alter table empleados add constraint fk_empleados_ramas_ejecutivas foreign key (rama_ejecutivaid) references ramas_ejecutivas (id);
+
+alter table empleados_proyectos add constraint fk_empleados_proyectos_proyectos foreign key (proyectoid) references proyectos (id);
+alter table empleados_proyectos add constraint fk_empleados_proyectos_empleados foreign key (empleadoid) references empleados (id);
+
+alter table recursos add constraint fk_recursos_proyectos foreign key (proyectoid) references proyectos (id);
+alter table recursos add constraint fk_recursos_unidad_medida foreign key (medidaid) references unidad_medidas (id);
+
+alter table alcances add constraint fk_alcances_actividades foreign key (actividadid) references actividades (id);
+alter table alcances add constraint fk_alcances_calificaciones foreign key (califid) references calificaciones (id);
+
+alter table costos add constraint fk_costos_alcances foreign key (alcanceid) references alcances (id);
+
+alter table adquisiciones add constraint fk_adquisiciones_alcances foreign key (alcanceid) references alcances (id);
+
+alter table gerente_proyecto add constraint fk_gerente_proyecto_gerentes foreign key (gerenteid) references gerentes (id);
+alter table gerente_proyecto add constraint fk_gerente_proyecto_proyectos foreign key (proyectoid) references proyectos (id);
